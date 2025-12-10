bcbe2960559a3160641522f447bc4ba123d64e9f
Renan <erenan257@gmail.com>
2025-12-10 13:31:47 -0300

    first commit

---
+++ Assets/NoteGameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NoteGameManager : MonoBehaviour
+{
+    public string notaCorreta;
+    public int pontuacao = 0;
+
+    public TMP_Text pontuacaoText;   // UI da pontuação
+    public TMP_Text mensagemText;    // UI da mensagem
+
+    public void NotaTocada(GameObject nota)
+    {
+        string nome = nota.name.ToLower();
+
+        if (nome == notaCorreta.ToLower())
+        {
+            pontuacao++;
+            if (mensagemText != null)
+                mensagemText.text = "Correto!";
+        }
+        else
+        {
+            if (mensagemText != null)
+                mensagemText.text = "Errado!";
+        }
+
+        if (pontuacaoText != null)
+            pontuacaoText.text = "Pontuação: " + pontuacao;
+    }
+}
---
+++ Assets/PlayNote.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayNote : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private NoteGameManager gameManager;
+
+    void Start()
+    {
+        gameManager = FindObjectOfType<NoteGameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (audioSource != null)
+            audioSource.Play();
+
+        if (gameManager != null)
+            gameManager.NotaTocada(gameObject);
+    }
+}
